1d3da9b14899785b4d91a5d1e776938a2c1b6e89
Chakrit W <chakrit@ucconnect.co.th>
2019-01-21 12:31:14 +0700

    Implement express webhook

--- main.js
+++ main.js
@@ -1,7 +1,54 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
+const debug = require("debug")("NodeChatBot:main");
+const util = require("util");
+const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const _pkgInfo = require("./package.json");
+
+let lineBotConf = _pkgInfo.LINEBot || {};
+console.log(`LINE Bot channel ID: ${lineBotConf.channelID}`);
+
+let app = express();
+
+app.use(morgan("combined"));
+//app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.post("/webhook", (req, resp) => {
+    let body = req.body;
+    console.log(`${req.url} =>`, body);
+    resp.json({
+        status: "ok",
+        body: body
+    });
+});
+
+app.get((req, resp, next) => {
+    let err = new Error("Not Found");
+    err.statusCode = 404;
+    next(err);
+});
+app.get((err, req, resp, next) => {
+    let statusCode = err.statusCode || 500;
+    (statusCode >= 500) && console.error(err);
+    resp.status(statusCode)
+      .json({ errorMessage: err.message || "ERROR" });
+});
 
 
+let port = process.env.PORT || 3000;
+
+app.listen(port, (err) => {
+   if (err) {
+       console.error(err);
+       process.exit(-1);
+   }
+   console.info(`App Listening on port: ${port}`);
+   console.info("Press Ctrl+C to quit");
+});
+
+process.on("SIGINT", (params) => {
+    console.warn("App terminating", params);
+    process.exit(0);
+});
